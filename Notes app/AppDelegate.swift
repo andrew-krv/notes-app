11cd2f24f1e8410324e6be5f8f5133f2a7508ed4
Andrii Kryvytskyi <andry.krivitskiy@gmail.com>
2019-11-14 16:31:24 +0200

    - Preparations for new UI for notes

--- Notes app/AppDelegate.swift
+++ Notes app/AppDelegate.swift
@@ -7,31 +7,99 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+    
+    var window: UIWindow?
 
 
+   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       // Override point for customization after application launch.
+       let splitViewController = window!.rootViewController as! UISplitViewController
+       let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+       navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+       splitViewController.delegate = self
+       return true
+   }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+   func applicationWillResignActive(_ application: UIApplication) {
+       // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+       // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+   }
 
-    // MARK: UISceneSession Lifecycle
+   func applicationDidEnterBackground(_ application: UIApplication) {
+       // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+       // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+   }
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
+   func applicationWillEnterForeground(_ application: UIApplication) {
+       // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+   }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+   func applicationDidBecomeActive(_ application: UIApplication) {
+       // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   }
 
+   func applicationWillTerminate(_ application: UIApplication) {
+       // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+   }
 
+   // MARK: - Split view
+   func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+       guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+       guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+       if topAsDetailController.detailItem == nil {
+           // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+           return true
+       }
+       return false
+   }
+
+   // MARK: - Core Data stack
+   
+   lazy var persistentContainer: NSPersistentContainer = {
+       /*
+        The persistent container for the application. This implementation
+        creates and returns a container, having loaded the store for the
+        application to it. This property is optional since there are legitimate
+        error conditions that could cause the creation of the store to fail.
+        */
+       let container = NSPersistentContainer(name: "ReallySimpleNotesData")
+       container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+           if let error = error as NSError? {
+               // Replace this implementation with code to handle the error appropriately.
+               // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+               
+               /*
+                Typical reasons for an error here include:
+                * The parent directory does not exist, cannot be created, or disallows writing.
+                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                * The device is out of space.
+                * The store could not be migrated to the current model version.
+                Check the error message to determine what the actual problem was.
+                */
+               fatalError("Unresolved error \(error), \(error.userInfo)")
+           }
+       })
+       return container
+   }()
+   
+   // MARK: - Core Data Saving support
+   
+   func saveContext () {
+       let context = persistentContainer.viewContext
+       if context.hasChanges {
+           do {
+               try context.save()
+           } catch {
+               // Replace this implementation with code to handle the error appropriately.
+               // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+               let nserror = error as NSError
+               fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+           }
+       }
+   }
 }
 
---
+++ Notes app/NoteClass.swift
@@ -0,0 +1,30 @@
+//
+//  NoteClass.swift
+//  Notes app
+//
+//  Created by Andrii Kryvytskyi on 14.11.2019.
+//  Copyright © 2019 Andrii Kryvytskyi. All rights reserved.
+//
+
+import Foundation
+
+class NoteClass {
+    private(set) var noteId        : UUID
+    private(set) var noteTitle     : String
+    private(set) var noteText      : String
+    private(set) var noteTimeStamp : Int64
+    
+    init(noteTitle:String, noteText:String, noteTimeStamp:Int64) {
+        self.noteId        = UUID()
+        self.noteTitle     = noteTitle
+        self.noteText      = noteText
+        self.noteTimeStamp = noteTimeStamp
+    }
+
+    init(noteId: UUID, noteTitle:String, noteText:String, noteTimeStamp:Int64) {
+        self.noteId        = noteId
+        self.noteTitle     = noteTitle
+        self.noteText      = noteText
+        self.noteTimeStamp = noteTimeStamp
+    }
+}
