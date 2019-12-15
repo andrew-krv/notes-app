caf983b3bbf8505757cfc25423864e50011cb1f0
Andrii Kryvytskyi <andry.krivitskiy@gmail.com>
2019-12-16 01:29:19 +0200

    - finished unit test for CoreData model

--- Notes appTests/CoreData_Tests.swift
+++ Notes appTests/CoreData_Tests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Notes_app
 
-class Notes_appTests: XCTestCase {
+class CoreData_appTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
